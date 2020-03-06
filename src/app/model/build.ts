910bc38cf9353aada02a91671f84d4abd6d4a7c6
u203965 <marc.walter@sbb.ch>
2020-03-06 11:40:16 +0100

    upgraded build model

--- src/app/model/build.ts
+++ src/app/model/build.ts
@@ -7,14 +7,9 @@ import {Culprit} from './culprit';
 export interface Build {
   building: boolean;
   description: string;
-  displayName: string;
   duration: number;
-  estimatedDuration: number;
   fullDisplayName: string;
-  id: string;
-  kepLog: boolean;
   number: number;
-  queueId: number;
   result: string;
   timestamp: number;
   url: string;
