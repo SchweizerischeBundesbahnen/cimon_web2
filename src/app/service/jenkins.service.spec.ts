994c8b7f7d07ba640715048cc0d94c61c77cd83f
u203965 <marc.walter@sbb.ch>
2020-03-06 12:08:36 +0100

    fix service tests

--- src/app/service/jenkins.service.spec.ts
+++ src/app/service/jenkins.service.spec.ts
@@ -5,9 +5,15 @@
 import {TestBed} from '@angular/core/testing';
 
 import {JenkinsService} from './jenkins.service';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {SettingsService} from './settings.service';
 
 describe('JenkinsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule, HttpClientTestingModule],
+    providers: [SettingsService]
+  }));
 
   it('should be created', () => {
     const service: JenkinsService = TestBed.get(JenkinsService);
